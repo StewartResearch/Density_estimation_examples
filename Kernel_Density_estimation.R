31ad66de95e748165b5d4ca4867753a2979c94c3
Frances Stewart <francesstewart@simplys-iMac-2.local>
2022-03-03 16:52:32 -0500

    started 2D KDE

--- Kernel_Density_estimation.R
+++ Kernel_Density_estimation.R
@@ -61,5 +61,52 @@ plot(kde2)
 ## what if our data is spatial (i.e. 2D?)
 ## we need to be able to spatially reference each data point so that we can plot our kernel density function on a map
 
+# Lets use the example of car accidents from Freakonomics
+# load this Rdata file from the following url:
+cardata<-load(url("https://egallic.fr/R/sKDE/smooth-maps/data/car_accidents/acci.RData"))
+cardata #acci is a list of 2 elements. See this in the Environments tab in R studio or:
+summary(acci)
+# these two elements are information on two departements in France
 
+# plot the loctions of one of the elements
+install.packages("ggplot2")
+library("ggplot2")
+
+#Finistere accident locations:
+ggplot() + 
+  geom_polygon(data = acci$finistere$polygon, 
+               mapping = aes(x = long, y = lat),
+               fill = "grey75") +
+  geom_point(data = acci$finistere$points, aes(x = long, y = lat), 
+             col = "dodger blue", alpha = .5) + coord_equal() +
+  ggtitle("Accidents in Finistere")
+# lat and long are along the axes so that we have spatial information for all the data points, pointedin Blue
+
+# what if we did a kernel density function of these points?
+acci$finistere$points # each points has both a lat and long, but other than that not a value for each one.
+# this means that we need to use a different function, as now we are using 2D data, rather than the simple "kernel()" used above.
+
+install.packages("sf")
+library(sf)
+install.packages("tidyverse")
+library(tidyverse)
+
+# we first need to be able to create a sf object to use the fucntions from {sf} to compute the intersection of the area
+
+polygon_finistere <-
+  cbind(
+    c(acci$finistere$polygon$long, acci$finistere$polygon$long[1]),
+    c(acci$finistere$polygon$lat, acci$finistere$polygon$lat[1])
+  ) %>%
+  list() %>%
+  sf::st_polygon()
+
+# now run an estimation of the data in this polygon with smoothed corrections using the sKDE()
+#source the sKDE()
+source("sKDE_function.R")
+smoothed_fin <- sKDE(U = acci$finistere$points, 
+                     polygon = polygon_finistere,
+                     optimal=TRUE, parallel = FALSE)
+# ----
+# issues here with the Hpi() - not sure where this is coming from. 
 
