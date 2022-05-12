ee004a27950144dda7beb7dc1859616c4df9fd62
Frances Stewart <francesstewart@simplys-iMac-2.local>
2022-05-12 11:27:37 -0400

    Practicals2 update

---
+++ RexstedWorkshop2022.R
@@ -0,0 +1,13 @@
+#######################################
+# Eric Rexstad Distance Sampling workshop materials from April 2022
+# Charlotte Rentmeister took this workshop
+# All information can be found on the StewartLab GoogleDrive folder, under Workshops -> Distance Workshop
+# Code below is me brining together the practicals into one repository
+########################################
+# Good resources:
+## Course website: http://distancelive.xyz 
+########################################
+# Started May 2022
+########################################
+# F. Stewart
+########################################
